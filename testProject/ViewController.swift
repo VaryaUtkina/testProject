68e31f683c7ed1f88df52ce8d940d8b2cdfc056e
Varya Utkina <varya@icloud.com>
2023-11-11 20:21:36 +0300

    Work with logic

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        if counter == 0 {
+            startButton.setTitle("NEXT", for: .normal)
+            greenLightView.backgroundColor = UIColor.init(
+                red: 48/255,
+                green: 209/255,
+                blue: 88/255,
+                alpha: 0.3
+            )
+            redLightView.backgroundColor = UIColor.red
+        } else if counter == 1 {
+            redLightView.backgroundColor = UIColor.init(
+                red: 255/255,
+                green: 38/255,
+                blue: 0/255,
+                alpha: 0.3
+            )
+            yellowLightView.backgroundColor = UIColor.yellow
+        } else if counter == 2 {
+            yellowLightView.backgroundColor = UIColor.init(
+                red: 255/255,
+                green: 251/255,
+                blue: 0/255,
+                alpha: 0.3
+            )
+            greenLightView.backgroundColor = UIColor.green
+            counter = 0
+            return
+        }
+        counter += 1
+    }
+    
 }
-
